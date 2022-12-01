ad5133b23b26fd2fe9f83cb2eac0483fba3b9baa
ginacollecchia <ginacollecchia@gmail.com>
2022-12-01 15:53:17 -0500

    Updated Twit to use twitter-v2 instead of twit.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key: process.env.TWITTYMIDI_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTYMIDI_CONSUMER_SECRET,
+  access_token: process.env.TWITTYMIDI_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTYMIDI_ACCESS_TOKEN_SECRET,
+  bearer_token: process.env.TWITTYMIDI_BEARER_TOKEN,
+  timeout_ms: 60*1000
+}
